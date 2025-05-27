2b07f0d6e3b17f2ea2497e45ae223b6ea2b4297a
nurealammiaji <nurealam.miaji@gmail.com>
2025-05-27 22:47:33 +0600

    vercel config added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("OMSC Server is running ....");
 })
+
 
 app.listen(port, () => {
     console.log(`OMSC Server is running on Port: ${port}`);
